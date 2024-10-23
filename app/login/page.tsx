90a77960073f2e144e2c6b4a28f11ecbdb4d7cea
Obisanya1 <ifeoluwadaniel25@gmail.com>
2024-10-23 22:14:46 +0100

    tried to add a heading

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image from 'next/image'
+
+const page = () => {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <Image alt='Logo' src="" width={400} height={400}/>
+    </div>
+  )
+}
+
+export default page
